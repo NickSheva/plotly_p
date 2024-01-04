42496d1d8be332420b8be7e3541706edb6ba939c
NickSheva <nikolayshevtsov@mail.ru>
2024-01-04 21:59:20 +0500

    add depends with import json to read .geojson to read

--- plotly_p/main.py
+++ plotly_p/main.py
@@ -1,11 +1,12 @@
+"""Извдечение таблицы из сайта wikipedia
+и сохрание в .csv file"""
 import pandas as pd
-import csv
-
+import json
 
 # Путь к таблице на сайте wikipedia
-url = "https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population"
+page = "https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population"
 # Чтение файла с помощью pandas
-data = pd.read_html(url)
+data = pd.read_html(page)
 # Находим количество таблиц на старанице
 print(f"Page has: {len(data)} table(s)")
 # Нам нужна таблица 1
@@ -15,10 +16,14 @@ table.to_excel('tab.xlsx')
 # Сохраняем таблицу в csv
 pd.DataFrame(table.to_csv('tab.csv', index=False))
 # Убираем первую строку из таблицы
-with open('tab.csv', 'r') as file, open('table.csv', 'w') as f_out:
+with open('tab.csv', 'r') as file:
+    with open('table.csv', 'w') as f_out:
         # Пропускаем первую строку
         next(file)
         for i in file:
             # Запись нового файла
             f_out.write(i)
-
+# Открываем файл geojson
+filename = './states_india.geojson'
+india_state = json.load(open(filename, 'r'))
+print(india_state)
